528c6e93ac3ad261f39dc092209821e5b3a0fc40
Dhiraj Sardar <sardardhiraj706@gmail.com>
2025-06-16 10:26:34 +0530

    added the simple api app to github

---
+++ src/test/java/com/simple/api/github/action/demo/github_action_demo/GithubActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.api.github.action.demo.github_action_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
